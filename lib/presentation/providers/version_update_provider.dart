b6d195f0d3ebd91823c239b34d782574ca90e3ee
0vethor0 <uchija.shinui@gmail.com>
2026-06-06 23:45:50 -0400

    fix: He eliminado la importación no utilizada de path_provider en el archivo version_update_provider.dart

--- lib/presentation/providers/version_update_provider.dart
+++ lib/presentation/providers/version_update_provider.dart
@@ -7,7 +7,6 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:open_filex/open_filex.dart';
 import 'package:package_info_plus/package_info_plus.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
